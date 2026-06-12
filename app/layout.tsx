b8b94160dc868641f3e57b00306c25c77698f213
tensynavi <gaoquaidou1@gmail.com>
2026-06-12 19:24:27 +0900

    Delete app/layout.tsx

--- app/layout.tsx
+++
@@ -1,9 +0,0 @@
-import type { Metadata } from 'next'
-import './globals.css'
-
-export const metadata: Metadata = {
-  title: '転職Navi - あなたの理想の仕事が見つかる',
-  description: '都道府県・職種・キーワードで近くの求人をすぐに検索できる転職サイト',
-}
-
-export default function RootLayout({
